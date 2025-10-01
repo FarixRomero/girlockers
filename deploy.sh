8937bb0c5c48ec224c841a1099e82b723296241d
FarixRomero <farixromero123@gmail.com>
2025-09-30 20:30:37 -0500

    Agrega archivos de build al repositorio

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ npm run build
 # Commit y push de los archivos de build
 git add .
 git commit -m "Agrega archivos de build al repositorio" || true
-git push origin main
+git push origin master
 
 # Conectarse al servidor y ejecutar comandos remotos
 wsl sshpass -p "Trilce@123" ssh -p 65002 -o StrictHostKeyChecking=no u912353527@89.116.115.104 << 'EOF'
@@ -20,7 +20,7 @@ wsl sshpass -p "Trilce@123" ssh -p 65002 -o StrictHostKeyChecking=no u912353527@
 
 
   echo "📦 Ejecutando git pull..."
-  git pull origin main
+  git pull origin master
   php artisan migrate:refresh --seed
 
   echo "✅ Deployment completado!"

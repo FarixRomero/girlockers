2894b8a50d4c31c55cc0a25255dc89c9c62eb99e
Farix Romero <farixromer123@gmail.com>
2026-02-13 10:50:03 -0500

    Avance

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,35 +34,35 @@ class DatabaseSeeder extends Seeder
         $this->call(InstructorSeeder::class);
 
         // Create real courses with CourseSeeder
-        $this->call(CourseSeeder::class);
+        // $this->call(CourseSeeder::class);
 
         $this->call(LandingConfigSeeder::class);
         // Create test students
-        $students = User::factory(10)->create();
-        $premiumStudents = User::factory(5)->withFullAccess()->create();
+        // $students = User::factory(10)->create();
+        // $premiumStudents = User::factory(5)->withFullAccess()->create();
 
-        // Get all lessons to add comments and likes
-        $lessons = Lesson::all();
+        // // Get all lessons to add comments and likes
+        // $lessons = Lesson::all();
 
-        foreach ($lessons as $lesson) {
-            // Add some comments to lessons
-            if (rand(0, 1)) {
-                Comment::factory(rand(1, 5))->create([
-                    'lesson_id' => $lesson->id,
-                    'user_id' => $students->random()->id,
-                ]);
-            }
+        // foreach ($lessons as $lesson) {
+        //     // Add some comments to lessons
+        //     if (rand(0, 1)) {
+        //         Comment::factory(rand(1, 5))->create([
+        //             'lesson_id' => $lesson->id,
+        //             'user_id' => $students->random()->id,
+        //         ]);
+        //     }
 
-            // Add some likes to lessons
-            $likesCount = rand(0, 10);
-            if ($likesCount > 0) {
-                $likers = $students->random(min($likesCount, $students->count()));
-                foreach ($likers as $liker) {
-                    $lesson->likes()->attach($liker->id);
-                }
-                $lesson->update(['likes_count' => $likesCount]);
-            }
-        }
+        //     // Add some likes to lessons
+        //     $likesCount = rand(0, 10);
+        //     if ($likesCount > 0) {
+        //         $likers = $students->random(min($likesCount, $students->count()));
+        //         foreach ($likers as $liker) {
+        //             $lesson->likes()->attach($liker->id);
+        //         }
+        //         $lesson->update(['likes_count' => $likesCount]);
+        //     }
+        // }
 
         $this->command->info('Database seeded successfully!');
         $this->command->info('Admin credentials: admin@girlslockers.com / password');
